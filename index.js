f5d9dc4271cbba7d519d5a3a1f9b4cb9370e5c06
Isaac-Machini <isaac.machini@student.moringaschool.com>
2022-08-25 09:05:58 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'jane'
+var bestcustomer;
+
+  function upperCaseCustomerName(){  
+ customerName = customerName.toUpperCase()
+  };
+
+  function setBestCustomer(){
+    return bestcustomer = 'not bob'
+  }
+
+  function overWriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+    function changesLeastFavoriteCustomer(){
+      return leastFavoriteCustomer = 'Jude'
+    }
+  }
+
+
+
+    
